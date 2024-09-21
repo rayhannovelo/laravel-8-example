57cd64b8558828dd850148c251f2f1a894571739
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-09-21 08:43:14 +0700

    initial commit

---
+++ tests/Feature/Livewire/FormTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Http\Livewire\Form;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class FormTest extends TestCase
+{
+    /** @test */
+    public function the_component_can_render()
+    {
+        $component = Livewire::test(Form::class);
+
+        $component->assertStatus(200);
+    }
+}
